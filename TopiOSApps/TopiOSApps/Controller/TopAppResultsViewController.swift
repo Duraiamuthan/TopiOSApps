41749009b45e70b82b4ef4d9a6a829443aba0a53
Duraiamuthan Harikrishnan <duraiamuthan.harikrishnan@bt.com>
2020-03-30 23:07:49 +0100

    - Organised the files - Improved the code ( spacing ) , refactoring the optional wrapping with if let

--- TopiOSApps/TopiOSApps/Controller/TopAppResultsViewController.swift
+++ TopiOSApps/TopiOSApps/Controller/TopAppResultsViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class TopAppResultsViewController: UIViewController,UISearchBarDelegate,UITableViewDataSource,UITableViewDelegate {
-
+    
     @IBOutlet weak var searchBariOSAppFilter: UISearchBar!
     @IBOutlet weak var tblViewiOSAppFilterResults: UITableView!
     @IBOutlet weak var segmentControlTopApps: UISegmentedControl!
     var refreshControl:UIRefreshControl!
-    var apps:[Entry]?,appsOriginal:[Entry]?
+    var filteredAppList:[Entry]?,appsList:[Entry]?
     
     
     override func viewDidLoad() {
@@ -30,11 +30,11 @@ class TopAppResultsViewController: UIViewController,UISearchBarDelegate,UITableV
         self.configurePullToRefresh()
         
         // setting delegate for search bar
-         searchBariOSAppFilter.delegate=self
+        searchBariOSAppFilter.delegate = self
         
         // setting delegate for tableview
-        tblViewiOSAppFilterResults.dataSource=self
-        tblViewiOSAppFilterResults.delegate=self
+        tblViewiOSAppFilterResults.dataSource = self
+        tblViewiOSAppFilterResults.delegate = self
         
         // get the app data
         self.getData(refersh: false)
@@ -55,13 +55,13 @@ class TopAppResultsViewController: UIViewController,UISearchBarDelegate,UITableV
         
         //Adding observer for data error
         nc.addObserver(self, selector: #selector(onDataError(_:)), name: Notification.Name(AppConstants.errorWithDataNotificationName), object: nil)
-
+        
     }
     
     // MARK: - Segues
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let indexPath = self.tblViewiOSAppFilterResults.indexPathForSelectedRow {
-             let object:Entry? = self.apps?[indexPath.row]
+            let object = self.filteredAppList?[indexPath.row]
             let appDetailVC = segue.destination as? AppDetailViewController
             appDetailVC?.detailText = object?.summary.label
         }
@@ -71,7 +71,7 @@ class TopAppResultsViewController: UIViewController,UISearchBarDelegate,UITableV
     
     // For handling network related error
     @objc func onErrorFetchingServerData(_ notification:Notification) {
-        let err:Error? =  notification.object as? Error
+        let err =  notification.object as? Error
         
         let errorCode = (err as NSError?)?.code
         
@@ -81,7 +81,7 @@ class TopAppResultsViewController: UIViewController,UISearchBarDelegate,UITableV
         
         alert.addAction(UIAlertAction(title: AppConstants.errorAlertButton, style: UIAlertAction.Style.default, handler: nil))
         
-         DispatchQueue.main.async() {
+        DispatchQueue.main.async() {
             self.present(alert, animated: true, completion: nil)
         }
     }
@@ -89,15 +89,15 @@ class TopAppResultsViewController: UIViewController,UISearchBarDelegate,UITableV
     //For handling data related error
     @objc func onDataError(_ notification:Notification) {
         let alert = UIAlertController(title: AppConstants.errorAlertTitle, message: AppConstants.errorMessageData, preferredStyle: .alert)
-               
-               alert.addAction(UIAlertAction(title: AppConstants.errorAlertButton, style: UIAlertAction.Style.default, handler: nil))
-               
-                DispatchQueue.main.async() {
-                   self.present(alert, animated: true, completion: nil)
-               }
+        
+        alert.addAction(UIAlertAction(title: AppConstants.errorAlertButton, style: UIAlertAction.Style.default, handler: nil))
+        
+        DispatchQueue.main.async() {
+            self.present(alert, animated: true, completion: nil)
+        }
     }
-
-   
+    
+    
     // MARK: - Data for tableview
     
     // It brings the app related data
@@ -108,35 +108,35 @@ class TopAppResultsViewController: UIViewController,UISearchBarDelegate,UITableV
         }
         
         GetAppCatalog.GetListOfApps { Entries in
-             self.apps=Entries;
-             self.appsOriginal=Entries;
-             DispatchQueue.main.async() {
+            self.filteredAppList=Entries;
+            self.appsList=Entries;
+            DispatchQueue.main.async() {
                 self.view.endEditing(true)
                 // if pull to refresh is enabled stop it
                 if refersh {
                     self.refreshControl?.endRefreshing()
                 }
                 else {
-                //Hide Spinner
-                self.removeSpinner()
+                    //Hide Spinner
+                    self.removeSpinner()
                 }
                 //reload the table view
                 self.tblViewiOSAppFilterResults.reloadData()
-             }
-         };
+            }
+        };
     }
     
     // It filters the data based on searcy key
     func filter(searchKey:String) -> () {
-
+        
         if searchKey.trim().count == 0 {
-            apps = self.appsOriginal
+            filteredAppList = self.appsList
         }
         else if segmentControlTopApps.selectedSegmentIndex == 0 {
-            apps = self.appsOriginal?.filter({$0.imName.label.localizedCaseInsensitiveContains(searchKey.trim())})
+            filteredAppList = self.appsList?.filter({$0.imName.label.localizedCaseInsensitiveContains(searchKey.trim())})
         }
         else{
-            apps = self.appsOriginal?.filter({$0.category.attributes.label.localizedCaseInsensitiveContains(searchKey.trim())})
+            filteredAppList = self.appsList?.filter({$0.category.attributes.label.localizedCaseInsensitiveContains(searchKey.trim())})
         }
         self.tblViewiOSAppFilterResults.reloadData()
     }
@@ -150,18 +150,18 @@ class TopAppResultsViewController: UIViewController,UISearchBarDelegate,UITableV
     // to configure pulll to refresh for current table view
     func configurePullToRefresh(){
         self.refreshControl = UIRefreshControl()
-         refreshControl?.addTarget(self, action: #selector(refresh(sender:)), for: UIControl.Event.valueChanged)
-         self.tblViewiOSAppFilterResults.addSubview(refreshControl!)
+        refreshControl?.addTarget(self, action: #selector(refresh(sender:)), for: UIControl.Event.valueChanged)
+        self.tblViewiOSAppFilterResults.addSubview(refreshControl!)
     }
     
     // pull to refresh target function
     @objc func refresh(sender:AnyObject) {
-           self.getData(refersh: true)
+        self.getData(refersh: true)
     }
     
     // MARK: - Searchbar methods
-   func searchBar(_ searchBar: UISearchBar,
-                  textDidChange searchText: String){
+    func searchBar(_ searchBar: UISearchBar,
+                   textDidChange searchText: String){
         if let key = searchBar.text?.trim() {
             self.filter(searchKey: key)
         }
@@ -186,7 +186,7 @@ class TopAppResultsViewController: UIViewController,UISearchBarDelegate,UITableV
         //hide the keyboard
         searchBar.resignFirstResponder()
         
-        //
+        //empty the search key and search bar
         searchBar.text=""
         self.filter(searchKey: "")
     }
@@ -201,7 +201,7 @@ class TopAppResultsViewController: UIViewController,UISearchBarDelegate,UITableV
         case 0:
             searchBariOSAppFilter.placeholder=AppConstants.searchPlaceHolderOne
         case 1:
-             searchBariOSAppFilter.placeholder=AppConstants.searchPlaceHolderTwo
+            searchBariOSAppFilter.placeholder=AppConstants.searchPlaceHolderTwo
         default:
             searchBariOSAppFilter.placeholder=AppConstants.searchPlaceHolderDefault
         }
@@ -209,45 +209,49 @@ class TopAppResultsViewController: UIViewController,UISearchBarDelegate,UITableV
     
     // MARK: - Tableview methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.apps?.count ?? 0;
+        return self.filteredAppList?.count ?? 0;
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Bringing the customised cell
         let cell = tableView.dequeueReusableCell(withIdentifier: AppConstants.customTableViewCellId, for: indexPath) as! AppDetailTableViewCell
         
         // Bring the data for the particular index
-        let appMeta:Entry? = self.apps?[indexPath.row]
+        let appMeta = self.filteredAppList?[indexPath.row]
         
-        // Setting app name
-        cell.lblAppName!.text = appMeta?.imName.label
-        
-        // Check if there are any images
-        if((appMeta?.imImage.count)!>0){
+        if let appMeta = appMeta {
             
-        //Setting image height from API results
-        let heightOfImage:String = appMeta?.imImage[0].attributes.height ?? "53"
-        cell.setImageHeight(height: Float(heightOfImage)!)
-
-        // Setting placeholder image for lazing loading of images
-        let placeholderImage: UIImage = UIImage(named: AppConstants.lazyLoadingDefaultImageName)!
-        
-        cell.imgAppIcon.image = placeholderImage
-
-        // Download the image
-        cell.imgAppIcon.downloadImageFrom(link: appMeta?.imImage[0].label, contentMode: UIView.ContentMode.scaleAspectFit)
-        
+            // Setting app name
+            cell.lblAppName!.text = appMeta.imName.label
+            
+            // Check if there are any images
+            if( appMeta.imImage.count > 0 ){
+                
+                //Setting image height from API results
+                let heightOfImage:String = appMeta.imImage[0].attributes.height
+                
+                cell.setImageHeight(height: Float(heightOfImage) ?? 53.0)
+                
+                // Setting placeholder image for lazing loading of images
+                let placeholderImage: UIImage = UIImage(named: AppConstants.lazyLoadingDefaultImageName)!
+                
+                cell.imgAppIcon.image = placeholderImage
+                
+                // Download the image
+                cell.imgAppIcon.downloadImageFrom(link: appMeta.imImage[0].label, contentMode: UIView.ContentMode.scaleAspectFit)
+                
+            }
         }
         
         // Dark mode support
         if traitCollection.userInterfaceStyle == .dark {
-          cell.lblAppName.backgroundColor = .black
+            cell.lblAppName.backgroundColor = .black
         } else {
-          cell.lblAppName.backgroundColor = .white
-          cell.backgroundColor = .white
+            cell.lblAppName.backgroundColor = .white
+            cell.backgroundColor = .white
         }
-    
+        
         return cell
     }
-
+    
 }
